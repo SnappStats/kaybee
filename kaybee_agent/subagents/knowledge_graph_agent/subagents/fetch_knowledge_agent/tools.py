1476abb231acd28b5f16842891390b440b8582e7
Tim Eller <tseller@gmail.com>
2025-09-11 17:12:20 -0700

    Mark which entities are "valence", connected to entities outside the relevant subgraph.

--- kaybee_agent/subagents/knowledge_graph_agent/subagents/fetch_knowledge_agent/tools.py
+++ kaybee_agent/subagents/knowledge_graph_agent/subagents/fetch_knowledge_agent/tools.py
@@ -65,7 +65,7 @@ def get_relevant_neighborhood(entity_names: list[str], tool_context: ToolContext
         entity_names (list[str]): A list of entity names, and any synonyms, that might be nodes in the existing knowledge graph.
 
     Returns:
-        dict: A relevant portion of the knowledge graph.
+        dict: A relevant subgraph of the knowledge graph, including a surrounding neighborhood of the relevant entities (to help patching in a replacement subgraph).
     """
     graph_id = tool_context._invocation_context.user_id
     g = _fetch_knowledge_graph(graph_id=graph_id)
@@ -76,13 +76,23 @@ def get_relevant_neighborhood(entity_names: list[str], tool_context: ToolContext
     ])
     mdg = _knowledge_graph_to_nx(g)
 
+    # Relevant entities' 1-hop neighbors.
     nbrs1 = {
             nbr for entity_id in relevant_entity_ids
             for nbr in mdg.to_undirected().neighbors(entity_id)
+            if nbr not in relevant_entity_ids
     }
+    # Relevant entities' 2-hop neighbors.
     nbrs2 = {
             nbr for entity_id in nbrs1
             for nbr in mdg.to_undirected().neighbors(entity_id)
+            if nbr not in relevant_entity_ids and nbr not in nbrs1
+    }
+
+    # 2-hop neighbors connected to at least one external entity
+    valence_entities = {
+            entity_id for entity_id in nbrs2
+            if set(mdg.to_undirected().neighbors(entity_id)) - relevant_entity_ids - nbrs1 - nbrs2
     }
 
     neighborhood = mdg.subgraph(relevant_entity_ids|nbrs1|nbrs2)
@@ -91,7 +101,9 @@ def get_relevant_neighborhood(entity_names: list[str], tool_context: ToolContext
     # Reformat
     neighborhood = {
         'entities': {
-            node['entity_id']: node for node in neighborhood_json['nodes']},
+            node['entity_id']: dict(**node, frozen=(node['entity_id'] in valence_entities))
+            for node in neighborhood_json['nodes']
+        },
         'relationships': [
             {
                 'source_entity_id': link['source'],
